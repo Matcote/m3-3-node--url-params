cd1ab13f21d792993770094558d898739c7fc429
Matcote <matthewcotesipod@gmail.com>
2020-07-06 21:33:11 -0400

    workshop 100% done

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const express = require("express");
 
 const { top50 } = require("./data/top50");
+const { books } = require("./data/books");
 
 const PORT = process.env.PORT || 8000;
 
@@ -21,6 +22,7 @@ app.get("/top50", (req, res) => {
     title: "Top 50 Songs Streamed on Spotify",
     path: req.originalUrl,
     top50: top50,
+    id: -1,
   });
 });
 app.get("/top50/popular-artist", (req, res) => {
@@ -29,6 +31,7 @@ app.get("/top50/popular-artist", (req, res) => {
     title: "Most Popular Artist",
     path: req.originalUrl,
     top50: top50,
+    id: -1,
   });
 });
 app.get("/top50/song/:id", (req, res) => {
@@ -45,6 +48,49 @@ app.get("/top50/song/:id", (req, res) => {
     res.render("pages/fourOhFour", {
       title: "I got nothing",
       path: req.originalUrl,
+      id: -1,
+    });
+  }
+});
+
+//books not songs
+app.get("/books", (req, res) => {
+  res.status(200);
+  res.render("pages/books", {
+    title: "Books",
+    path: req.originalUrl,
+    books: books,
+    id: -1,
+  });
+});
+app.get("/books/:id", (req, res) => {
+  if (
+    req.params.id === "fiction" ||
+    req.params.id === "graphic-novel" ||
+    req.params.id === "non-fiction" ||
+    req.params.id === "drama"
+  ) {
+    res.status(200);
+    res.render("pages/bookGenre", {
+      title: req.params.id,
+      path: req.originalUrl,
+      books: books,
+      id: -1,
+    });
+  } else if (req.params.id > 100 && req.params.id < 126) {
+    res.status(200);
+    res.render("pages/bookPage", {
+      title: books[req.params.id - 101].title,
+      path: req.originalUrl,
+      books: books,
+      id: req.params.id - 101,
+    });
+  } else {
+    res.status(404);
+    res.render("pages/fourOhFour", {
+      title: "I got nothing",
+      path: req.originalUrl,
+      id: -1,
     });
   }
 });
@@ -55,6 +101,7 @@ app.get("*", (req, res) => {
   res.render("pages/fourOhFour", {
     title: "I got nothing",
     path: req.originalUrl,
+    id: -1,
   });
 });
 
